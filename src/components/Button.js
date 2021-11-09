a227986c891cd2b0fcd8d8e04d04f529552622d7
Iryna <irasokolova25@ukr.net>
2021-11-09 23:33:49 +0200

    Rename file

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Button extends Component{
+    render() {
+        return (
+            <button type='button' className='Button'>
+                Load more
+            </button>
+        )
+    }
+}
+export default Button
